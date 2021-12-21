b43142b9cfcb597f55d884086f261e29b45f9e76
Odaviing <tes5alduin@gmail.com>
2021-12-21 13:54:50 +0200

    init

---
+++ ConsoleApp5/Calculator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    public class Calculator
+    {
+       
+
+        public static double Sum(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Mult(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("You cannot divide on zero");
+            }
+            else
+            {
+                return a / b;
+            }
+            
+        }
+
+        public static double Square(double a)
+        {
+            if (a < 0)
+            {
+                throw new ArgumentOutOfRangeException("You cannot get a square root from negative number");
+            }
+            else
+            { 
+                return Math.Sqrt(a); 
+            }
+        }
+
+        
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input first number");
+            string str_first = Console.ReadLine();
+            double first, second;
+            if(!Double.TryParse(str_first, out first))
+            {
+                throw new ArgumentException("You must enter a number");
+            }
+            //first = Convert.ToDouble(str_first);
+
+            Console.WriteLine("Input second number");
+            string str_second = Console.ReadLine();
+            if (!Double.TryParse(str_second, out second))
+            {
+                throw new ArgumentException("You must enter a number");
+            }
+            //second = Convert.ToDouble(str_second);
+
+            Console.WriteLine("Choose the operation:\n 1. Sum\n 2. Subtract\n 3. Multiply\n 4. Divide\n 5. Square");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    Calculator.Sum(first, second);
+                    break;
+                case "2":
+                    Calculator.Subtract(first, second);
+                    break;
+                case "3":
+                    Calculator.Mult(first, second);
+                    break;
+                case "4":
+                    Calculator.Divide(first, second);
+                    break;
+                case "5":
+                    Calculator.Square(first);
+                    break;
+                default:
+                    throw new Exception("Incorrect option was chosen");
+                    
+
+            }
+
+        }
+    }
+}
